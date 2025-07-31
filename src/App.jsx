38f8d0c8d0061f496d255097446465da18f0c97b
amirhamidy <programing.amirhamidy@gmail.com>
2025-07-31 14:31:21 +0430

    Debugging lowercase and uppercase letters

--- src/App.jsx
+++ src/App.jsx
@@ -53,11 +53,11 @@ function App() {
       <div className='top-header-form'>
         <form className='row container mb-3'>
           <div className='col-md-10  py-3'>
-            <input className='form-control' type="text" placeholder='insert your work' />
+            <input className='form-control' type="text" placeholder='INSERT YOUR WORK' />
           </div>
           <div className='col-md-2 text-center py-3'>
             <button className='bg-success btn text-light' type='submit'>
-              ثبت
+              SUBMIT
             </button>
           </div>
         </form>
