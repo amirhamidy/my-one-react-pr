85e55b54d96c0ea89ae7ee162a159dbc1173af6b
amirhamidy <programing.amirhamidy@gmail.com>
2025-08-05 13:11:29 +0430

    add new work to todo app

--- src/form.jsx
+++ src/form.jsx
@@ -1,20 +1,35 @@
-import { StrictMode, useContext } from 'react'
+import { StrictMode, useContext, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-
-
-
+import Mycont from './myContext'
 
 
 const WorkForm = () => {
+
+    const { NewWorkString, SetNewWorkString } = useState("")
+    const {Works , SetWorks} = useContext(Mycont)
+
+    const ChangeNewWork = (ev)=>{
+        SetNewWorkString(ev.target.value)
+    }
+
+    const AddNewWork = (event)=>{
+
+        event.preventDefault()
+        SetWorks([... Works ,{id : Math.random(), WorksString : NewWorkString , IsDone : false } ])
+    }
+    
+
+
+
     return (
         <form className='row container mb-3'>
             <div className='col-md-10  py-3'>
-                <input className='form-control' type="text" placeholder='INSERT YOUR WORK' />
+                <input className='form-control' type="text" placeholder='INSERT YOUR WORK' value={NewWorkString} onChange={ChangeNewWork}/>
             </div>
             <div className='col-md-2 text-center py-3'>
-                <span className='bg-success btn text-light'>
+                <button type='submit' onClick={AddNewWork} className='bg-success btn text-light'>
                     SUBMIT
-                </span>
+                </button>
             </div>
         </form>
     )
