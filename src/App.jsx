fc874d7111c45eeef5b6fbff0264d33e1f129353
amirhamidy <programing.amirhamidy@gmail.com>
2025-09-07 19:56:17 +0430

    add progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './index.css'
 import SideBarMenu from './sidebar'
 import Navbar from './navbar'
 import NavContax from './myContaxt/contaxt'
-import Security from './pages/security'
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
+NProgress.configure({ showSpinner: false, speed: 500, trickleSpeed: 200 })
 
+function ProgressHandler() {
+  const location = useLocation()
+
+  useEffect(() => {
+    NProgress.start()
+    const timer = setTimeout(() => {
+      NProgress.done()
+    }, 50)
+    return () => clearTimeout(timer)
+  }, [location.pathname])
+
+  return null
+}
 
 function App() {
-
-
   const { ChangeProfile, SetChangeProfile } = useState('')
 
-
-
   return (
-
     <BrowserRouter>
-      <NavContax.Provider value={{
-        ChangeProfile, SetChangeProfile
-      }}>
+      <NavContax.Provider value={{ ChangeProfile, SetChangeProfile }}>
+        <ProgressHandler />
         <main className='row'>
           <SideBarMenu></SideBarMenu>
           <Navbar></Navbar>
@@ -29,6 +38,5 @@ function App() {
     </BrowserRouter>
   )
 }
-
 
 export default App
