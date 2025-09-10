22869838850c401d70d1fe52005a018adf2b6429
amirhamidy <programing.amirhamidy@gmail.com>
2025-09-10 23:53:51 +0430

    add API

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -1,43 +1,38 @@
 import { Link } from "react-router-dom";
 import UserForm from "./userForm";
+import axios from "axios";
+import { useState } from "react";
+
 
 const Users = () => {
-    const UsersData = [
-        { UserCode: "1", UserEmail: "test1@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "علی", UserPhone: "09380000001", UserLevel: "کاربر عادی" },
-        { UserCode: "2", UserEmail: "test2@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "مریم", UserPhone: "09380000002", UserLevel: "ادمین" },
-        { UserCode: "3", UserEmail: "test3@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "حسین", UserPhone: "09380000003", UserLevel: "کاربر عادی" },
-        { UserCode: "4", UserEmail: "test1@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "علی", UserPhone: "09380000001", UserLevel: "کاربر عادی" },
-        { UserCode: "5", UserEmail: "test2@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "مریم", UserPhone: "09380000002", UserLevel: "ادمین" },
-        { UserCode: "6", UserEmail: "test3@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "حسین", UserPhone: "09380000003", UserLevel: "کاربر عادی" },
-        { UserCode: "7", UserEmail: "test1@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "علی", UserPhone: "09380000001", UserLevel: "کاربر عادی" },
-        { UserCode: "9", UserEmail: "test2@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "مریم", UserPhone: "09380000002", UserLevel: "ادمین" },
-        { UserCode: "10", UserEmail: "test3@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "حسین", UserPhone: "09380000003", UserLevel: "کاربر عادی" },
-        { UserCode: "11", UserEmail: "test3@gmail.com", Useravatar: 'src/assets/img/m-main-prof.png', UserName: "حسین", UserPhone: "09380000003", UserLevel: "کاربر عادی" },
-    ];
+    const [UsersData, SetUsersData] = useState([])
+
+    axios.get('https://api.github.com/users').then(
+        res => SetUsersData(res.data)
+    )
 
     return (
         <>
 
-        
 
-        <UserForm></UserForm>
+
+            <UserForm></UserForm>
 
             <div className="d-flex justify-content-center flex-wrap cs-h-for-pr py-4">
                 {UsersData.map((user) => (
-                    <div key={user.UserCode} className="user-card d-flex flex-wrap justify-content-center align-items-center product-card  mx-2 px-3 py-3 my-2">
+                    <div key={user.id} className="user-card d-flex flex-wrap justify-content-center align-items-center product-card  mx-2 px-3 py-3 my-2">
                         <div className="mx-3 box-img-story">
                             <img
-                                src={user.Useravatar}
+                                src={user.avatar_url}
                                 className="img-story"
                             />
                         </div>
 
                         <div className="user-info w-100 text-center">
-                            <p className="my-2  one-line "><strong>کد کاربر:</strong> {user.UserCode}</p>
-                            <p className="my-2  one-line "><strong>ایمیل:</strong> {user.UserEmail}</p>
-                            <p className="my-2  one-line "><strong>نام:</strong> {user.UserName}</p>
-                            <p className="my-2  one-line "><strong>شماره:</strong> {user.UserPhone}</p>
-                            <p className="mb-0 user-level one-line ">{user.UserLevel}</p>
+                            <p className="my-2  one-line "><strong>کد کاربر:</strong> {user.node_id}</p>
+                            <p className="my-2  one-line "><strong>ایمیل:</strong> {user.type}</p>
+                            <p className="my-2  one-line "><strong>نام:</strong> {user.login}</p>
+                            <p className="my-2  one-line "><strong>شماره:</strong> {user.node_id}</p>
                         </div>
                         <Link to='/users/details' className="edit-btn border-none rounded-2 cs-fs-14 px-2 py-2 mt-3">مشاهده تمام جزئیات</Link>
                     </div>
